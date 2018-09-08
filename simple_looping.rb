eb60c2901584d5e65caa56ad9f4a98a3933ea984
Stephen Courtier <steves0089@gmail.com>
2018-09-08 15:57:25 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -13,10 +13,11 @@ def loop_iterator(number_of_times)
   counter = 0
   phrase = "Welcome to Flatiron School's Web Development Course!"
   loop do
-      counter =+1
+      
       if counter == number_of_times
         break
       else puts phrase
+        counter =+1
   end
 end
 
